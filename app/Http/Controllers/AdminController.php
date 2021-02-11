d3ee4b311477ea45bc36fca92e534aa35d800fa7
vagrant <vagrant@homestead>
2021-02-11 15:55:16 +0000

    Migration for descriptions, bug fixes, starting admin panel blade

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,17 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
+use App\Models\Description;
 
 class AdminController extends Controller
 {
-    public function index() {
-
-        return view('dashboard');
+    public function index()
+    {
+        $description = Description::find(1);
+        $about = json_decode($description->about);
+        return view('dashboard', compact('about'));
     }
 
-    public function add_about(request $request) {
+    public function add_about(request $request)
+    {
 
-        dd($request->all());
+        $array = [];
+        $array = $request->except('_method', '_token');
+        $about = new Description;
+        try {
+            $about = Description::find(1);
+            $about->about = json_encode($array);
+            $about->save();
+            return back()->with('success', 'done');
+        } catch (\Exception $exception) {
+            return back()->with('error', 'error');
+        }
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,22 @@
+@if ($message = Session::get('success'))
+    <div class="alert alert-success alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+@if ($message = Session::get('error'))
+    <div class="alert alert-danger alert-block">
+        <button type="button" class="close" data-dismiss="alert">×</button>
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+
 
     {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -12,11 +25,22 @@
             </div>
         </div>
     </div> --}}
-    <form action="{{route('add_about')}}" method="POST" class="contact_form" novalidate="novalidate" data-status="init">
+    @dump($about)
+    <form action="{{ route('add_about') }}" method="POST" class="contact_form" novalidate="novalidate"
+        data-status="init">
         @csrf
         @foreach (array_values(config('locale.languages')) as $language)
-            About -{{ $language[3] }}
-            <textarea name="about_{{ $language[0] }}"></textarea><br>
+            @if (property_exists($about, $language[0]))
+
+            @endif
+            About -{{ $language[3] }}_1
+            <textarea name="{{ $language[0] }}[]" value="{{ $language[0] }}"></textarea><br>
+
+            About -{{ $language[3] }}_2
+            <textarea name="{{ $language[0] }}[]" value="{{ $language[0] }}"></textarea><br>
+            About -{{ $language[3] }}_3
+            <textarea name="{{ $language[0] }}[]" value="{{ $language[0] }}"></textarea><br>
+
         @endforeach
         <input type="submit" value="OK" class="form-submit btn btn-primary">
     </form>
